6925107c0b974763f8da931b351f61b5997b029f
swayam <swayamkarle2@gmail.com>
2022-06-14 00:14:59 +0530

    15

---
+++ solutions/pr15_dsaSheet.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int sum = 0 ; // intialise sum 
+        int ans = 0 ;  // intialise answer to return 
+        int window  = 0 ;  // intialise window for calculation
+        int n = cardPoints.length ; 
+        for ( int i : cardPoints ){
+            sum += i ;
+        }
+        if(n == k){
+            return sum ;
+        }
+        for(int i = 0 ;i< n-k-1 ; i++){
+            window += cardPoints[i];
+        }
+        for(int i = n-k-1 ; i < n ; i++){
+            window += cardPoints[i];
+            ans =  Math.max(ans , sum - window);
+            window -= cardPoints[i-(n-k-1)];
+        }
+       return ans ;
+    }
+}
